ce8b08c42fefb73a79abf6d1c383441f12d1c16d
Amit <amit.maharxan@gmail.com>
2025-05-13 15:22:35 +0545

    added sharing url's in single page

--- wp-content/themes/carl_manlan/single.php
+++ wp-content/themes/carl_manlan/single.php
@@ -25,8 +25,21 @@ $post_type = get_post_type(); ?>
       <div class="blog-details pb-10">
 
         <div class="flex flex-wrap justify-between w-full">
-          <p class="added-on"><?php if (get_field("organization_post")) { ?>Originally Published on the <?php echo the_field("organization_post"); ?>,<?php } ?> <?php echo get_the_date(); ?> tent> </p>
-          <p class="flex gap-2 shareSocials">SHARE <img class="!my-auto" src="<?php echo DK_ASSEST_URI . '/icons/fb.svg'; ?>" alt="facebook"> <img class="!my-auto" src="<?php echo DK_ASSEST_URI . '/icons/xTwitter.svg' ?>" alt="X"> <img class="!my-auto" src="<?php echo DK_ASSEST_URI . '/icons/ln.svg' ?>" alt="Linkedin"> <img class="!my-auto" src="<?php echo DK_ASSEST_URI . '/icons/mail.svg' ?>" alt="Mail"> </p>
+          <p class="added-on"><?php if (get_field("organization_post")) { ?>Originally Published on the <?php echo the_field("organization_post"); ?>,<?php } ?> <?php echo get_the_date(); ?></p>
+          <p class="flex gap-2 shareSocials">SHARE 
+            <a id="fb-share" class="contents" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_permalink( get_the_ID() ) ); ?>">
+              <img class="!my-auto" src="<?php echo DK_ASSEST_URI . '/icons/fb.svg'; ?>" alt="facebook">
+            </a>
+            <a id="x-share" class="contents" target="_blank" href="https://twitter.com/share?url=<?php the_permalink();?>&via=<?php echo 'Carl_Manlan';?>&related=<?php echo urlencode( get_the_title() );?>&hashtags=<?php echo 'Media';?>&text=<?php echo urlencode( get_the_title() );?>">
+              <img class="!my-auto" src="<?php echo DK_ASSEST_URI . '/icons/xTwitter.svg' ?>" alt="X">
+            </a>
+            <a id="in-share" class="contents" target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>&text=<?php echo urlencode( get_the_title() );?>&summary=<?php echo urlencode( get_the_title() );?>&source=<?php echo 'Carl_Manlan';?>">
+              <img class="!my-auto" src="<?php echo DK_ASSEST_URI . '/icons/ln.svg' ?>" alt="Linkedin">
+            </a>
+            <a id="mail-share" class="contents" target="_blank" href="mailto:?subject=<?php echo rawurlencode( get_the_title() ); ?>&body=<?php echo rawurlencode( get_permalink() ); ?>">
+              <img class="!my-auto" src="<?php echo DK_ASSEST_URI . '/icons/mail.svg' ?>" alt="Mail">
+            </a>
+          </p>
         </div>
         <p class="desc"><?php the_content(); ?></p>
         <a class="btn-primary view-all uppercase" href="">Back To All</a>
