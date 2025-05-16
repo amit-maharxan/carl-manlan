11424be0b5eb0d00e051eaee54286e574f163ffa
Suyogya Luitel <lsuyogya@gmail.com>
2025-05-16 12:33:48 +0545

    txt col

--- wp-content/themes/carl_manlan/page-templates/template-policy.php
+++ wp-content/themes/carl_manlan/page-templates/template-policy.php
@@ -16,7 +16,7 @@ $url    = wp_get_attachment_url(get_post_thumbnail_id($postID), 'thumbnail'); ?>
     data-gradY="0"></div>
   <div class="after:absolute after:inset-0 after:bg-linear-to-bl after:from-dark/10 after:to-dark/80"></div>
   <div class=" container txtContent z-1 text-light pb-10">
-    <h1 class="text-xl/relaxed md:text-3xl/relaxed text-center py-20"><?php the_title(); ?></h1>
+    <h1 class="text-xl/relaxed md:text-3xl/relaxed text-center py-20 text-primary"><?php the_title(); ?></h1>
     <div class="content text-light/80 policyContent text-sm font-normal font-poppins">
       <?php the_content(); ?>
     </div>
