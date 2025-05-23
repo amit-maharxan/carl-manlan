312a1755af8e3841138e5969a41ea1d167f4d4fd
Amit <amit.maharxan@gmail.com>
2025-05-23 13:57:21 +0545

    minor

--- wp-content/themes/carl_manlan/template-parts/homepage/banner.php
+++ wp-content/themes/carl_manlan/template-parts/homepage/banner.php
@@ -83,7 +83,7 @@
 <section class="container my-20">
     <div class="flex justify-between mb-10 text-light">
         <h1 class="uppercase text-3xl"><?php the_field('hp_cards_title');?></h1>
-        <a href="<?php echo site_url('blueprint-for-success'); ?>" class="btn-primary !flex my-auto">View All</a>
+        <a href="<?php the_field('hp_cards_url');?>" class="btn-primary !flex my-auto">View All</a>
     </div>
     <div class="cardWrapper pb-8 grid grid-cols-[repeat(auto-fill,_minmax(350px,_1fr))] gap-12 uppercase font-medium">
         <?php if (have_rows('hp_cards')):
